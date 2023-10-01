207d761bf2a574ac9d2b6235e020da0f0807c30b
ChuangYueXiao <116137081+ChuangYueXiao@users.noreply.github.com>
2023-10-01 20:59:55 +0800

    Update SQLQuery3_3.sql

--- SQLQuery3_3.sql
+++ SQLQuery3_3.sql
@@ -1,2 +1,5 @@
 INSERT INTO Employee
-VALUES ('S0006', '�s�i���u','D010')
+VALUES ('S0006', '·s¶i­û¤u','D010')
+
+// 訊息 547，層級 16，狀態 0，行 1
+INSERT 陳述式與 FOREIGN KEY 條件約束 "FK__Employee__Em_cod__4E88ABD4" 衝突。衝突發生在資料庫 "MySchoolDB"，資料表 "dbo.Comp_dep", column 'Comp_dep_code'。
